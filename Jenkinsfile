e219db4e7ed2c52ae600de2bd23e9900ae6778e0
Joe O'Connor <joeoc2625@gmail.com>
2022-07-24 11:47:40 +0100

    Fix test pull issue

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,6 +1,12 @@
 pipeline {
   agent any
   stages {
+    stage('Pull Submodules') {
+      steps {
+        sh 'git pull --recurse-submodules '
+      }
+    }
+
     stage('Build') {
       steps {
         sh 'cargo build --package wasm-spirv'
--- test_gen/src/gen.rs
+++ test_gen/src/gen.rs
@@ -1,13 +1,17 @@
-use proc_macro2::{Punct, TokenStream, Ident};
-use syn::{ItemFn, LitStr, parse2};
-use syn::parse::{Parse, ParseStream};
-use syn::ext::IdentExt;
+use glob::glob;
+use proc_macro2::{Ident, Punct, TokenStream};
+use quote::{format_ident, quote};
 use std::env;
 use std::path::Path;
-use glob::glob;
-use quote::{format_ident, quote};
+use syn::ext::IdentExt;
+use syn::parse::{Parse, ParseStream};
 use syn::spanned::Spanned;
-use wast::{parser::{parse, ParseBuffer}, lexer::Lexer, Wast, WastDirective};
+use syn::{parse2, ItemFn, LitStr};
+use wast::{
+    lexer::Lexer,
+    parser::{parse, ParseBuffer},
+    Wast, WastDirective,
+};
 
 pub fn impl_tests(attr: TokenStream, f: ItemFn) -> TokenStream {
     let glob_str: syn::LitStr = parse2(attr).expect("unable to parse glob");
@@ -19,14 +23,22 @@ pub fn impl_tests(attr: TokenStream, f: ItemFn) -> TokenStream {
     let fn_name = f.sig.ident.clone();
 
     let mut tests = Vec::new();
-    for entry in glob(path.to_str().expect("failed to make glob")).expect("failed to read glob pattern") {
+    let test_files =
+        glob(path.to_str().expect("failed to make glob")).expect("failed to read glob pattern");
+    assert!(test_files.count() != 0, "found no test files");
+    for entry in test_files {
         let entry = entry.expect("error reading file");
 
-        let source = std::fs::read_to_string(entry.clone()).expect(&format!("could not read file {}", entry.to_str().unwrap()));
+        let source = std::fs::read_to_string(entry.clone())
+            .expect(&format!("could not read file {}", entry.to_str().unwrap()));
         let mut lexer = Lexer::new(&source);
         lexer.allow_confusing_unicode(true);
-        let buffer = ParseBuffer::new_with_lexer(lexer).expect(&format!("could not create parse buffer {}", entry.to_str().unwrap()));
-        let wast = parse::<Wast>(&buffer).expect(&format!("could not parse WAST {}", entry.to_str().unwrap()));
+        let buffer = ParseBuffer::new_with_lexer(lexer).expect(&format!(
+            "could not create parse buffer {}",
+            entry.to_str().unwrap()
+        ));
+        let wast = parse::<Wast>(&buffer)
+            .expect(&format!("could not parse WAST {}", entry.to_str().unwrap()));
 
         let mut i = 0u32;
         for kind in wast.directives {
@@ -38,12 +50,16 @@ pub fn impl_tests(attr: TokenStream, f: ItemFn) -> TokenStream {
                 | WastDirective::AssertExhaustion { .. }
                 | WastDirective::AssertUnlinkable { .. }
                 | WastDirective::AssertException { .. } => {
-                    let entry_name = entry.file_name()
+                    let entry_name = entry
+                        .file_name()
                         .expect("glob matched non-file")
-                        .to_str().unwrap()
+                        .to_str()
+                        .unwrap()
                         .to_string()
                         .split(".")
-                        .next().unwrap().to_string();
+                        .next()
+                        .unwrap()
+                        .to_string();
                     let entry_name = entry_name.replace("-", "_");
                     let test_name = format_ident!("{}_{}_{}", fn_name, entry_name, i);
                     let test_path_literal = LitStr::new(entry.to_str().unwrap(), f.span());
@@ -64,5 +80,5 @@ pub fn impl_tests(attr: TokenStream, f: ItemFn) -> TokenStream {
         #(#tests)*
 
         #f
-    }
+    };
 }
